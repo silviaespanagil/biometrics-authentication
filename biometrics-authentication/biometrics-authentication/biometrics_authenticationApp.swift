e2015900dba948754ee6b6fcbd5539b6c123b8df
Silvia Espana Gil <silviaespanagil@gmail.com>
2023-05-11 09:41:04 +0200

    🎉  First commit - Project created

---
+++ biometrics-authentication/biometrics-authentication/biometrics_authenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  biometrics_authenticationApp.swift
+//  biometrics-authentication
+//
+//  Created by Silvia España on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct biometrics_authenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
