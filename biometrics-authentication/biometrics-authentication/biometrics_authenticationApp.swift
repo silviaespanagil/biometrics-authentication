97407aae7ec9767844b6754c2c2102a1fb968a85
Silvia Espana Gil <silviaespanagil@gmail.com>
2023-05-12 09:18:45 +0200

    Refactor WindowGroup to render according AppState

--- biometrics-authentication/biometrics-authentication/biometrics_authenticationApp.swift
+++ biometrics-authentication/biometrics-authentication/biometrics_authenticationApp.swift
@@ -9,9 +9,20 @@ import SwiftUI
 
 @main
 struct biometrics_authenticationApp: App {
+    
+    @StateObject var appState = AppState()
+    
     var body: some Scene {
+        
         WindowGroup {
+            
+            ZStack {
+                if appState.loggedIn {
             ContentView()
+                 } else {
+                     LoginView(appState: appState)
+                 }
+            }
         }
     }
 }
