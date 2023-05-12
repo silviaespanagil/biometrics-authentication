ef8515ba6ce221b711d50073709c1cbbe0b7f074
Silvia Espana Gil <silviaespanagil@gmail.com>
2023-05-12 09:17:34 +0200

    Create AppState class to manage biometric methods and values

---
+++ biometrics-authentication/biometrics-authentication/AppState.swift
@@ -0,0 +1,78 @@
+//
+//  AppState.swift
+//  biometrics-authentication
+//
+//  Created by Silvia España on 11/5/23.
+//
+
+import Foundation
+import SwiftUI
+import LocalAuthentication
+
+class AppState: ObservableObject {
+    
+    @Published var loggedIn = false
+    @Published var authorizationError: Error?
+    
+    let context: LAContext = LAContext()
+    var canEvaluate = false
+    
+    func checkBiometricsAvailability() {
+        
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            
+            canEvaluate = context.biometryType != .none
+        }
+    }
+    
+    func requestPinAuthentication() {
+        
+        context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: "Enter your phone PIN to authenticate", reply: { (success, error) in
+            
+            DispatchQueue.main.async {
+                if success {
+                    self.loggedIn = true
+                    self.authorizationError = nil
+                } else {
+                    self.loggedIn = false
+                    self.authorizationError = error
+                }
+            }
+        })
+    }
+    
+    func requestBiometrics() {
+        
+        checkBiometricsAvailability()
+        
+        if canEvaluate {
+            // Authenticate using biometrics
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "To find the secret to Monkey Island") { (success, error) in
+                
+                DispatchQueue.main.async {
+                    
+                    if success {
+                        
+                        self.loggedIn = success
+                        self.authorizationError = error
+                    } else if let error = error as? LAError {
+                        
+                        switch error.code {
+                        case .authenticationFailed, .userCancel, .userFallback:
+                            
+                            self.requestPinAuthentication()
+                        default:
+                            
+                            self.loggedIn = false
+                        }
+                    } else {
+                        
+                        print(error ?? "")
+                    }
+                }
+            }
+        }
+    }
+}
